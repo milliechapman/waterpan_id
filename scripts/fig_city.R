1637cd1a0f05b42fee237108d3a3a7be3b52403e
Will Oestreich <woestreich@stanford.edu>
2018-07-17 15:25:11 +0200

    added code for recursions at pans plot

--- scripts/fig_city.R
+++ scripts/fig_city.R
@@ -48,8 +48,55 @@ plot(ele_wetRt$X,ele_wetRt$Y, col = "red", xlim=c(1040000, 1160000), ylim=c(-220
 # Maps showing potential pan centroids w/ circles for wet season 
 # recursions at these points overlaid. Reveals likely natural pan 
 # locations for (a) 2013, (b) 2014, and (c) 2015.
-
-
+# function to convert LL to UTM for recursion package
+LongLatToUTM<-function(x,y,zone){
+  xy <- data.frame(ID = 1:length(x), X = x, Y = y)
+  coordinates(xy) <- c("X", "Y")
+  proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")  ## for example
+  res <- spTransform(xy, CRS(paste("+proj=utm +zone=",zone," ellps=WGS84",sep='')))
+  return(as.data.frame(res))
+}
+# function to convert LL to UTM for recursion package
+LongLatToUTM<-function(x,y,zone){
+  xy <- data.frame(ID = 1:length(x), X = x, Y = y)
+  coordinates(xy) <- c("X", "Y")
+  proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")  ## for example
+  res <- spTransform(xy, CRS(paste("+proj=utm +zone=",zone," ellps=WGS84",sep='')))
+  return(as.data.frame(res))
+}
+# natural pans 
+pans<- readOGR("khaudum_data/khaudum_water/Khaudum_pans_pj.shp")
+# convert shapefile for pans and calculate centroids
+pans_ll <- spTransform(pans, CRS("+proj=longlat +datum=WGS84")) #change CRS
+centroid1<- as.data.frame(gCentroid(pans_ll, byid=TRUE))
+centroidUTM<-LongLatToUTM(centroid1$x, centroid1$y, 33)
+centroidUTM_2<-centroidUTM[,c(2,3)]
+# all artifical water sources
+art_water<-read.csv("khaudum_data/waterholes.csv")
+art_water_utm<-LongLatToUTM(art_water$Lon,art_water$Lat,33)
+#read in pan revisits and change low revisitation values to NA
+pan_revisits<-read.csv("outputs/pan_revisits_poly.csv")
+pan_revisits$revisits2013 <- ifelse(pan_revisits$revisits2013 > 1, pan_revisits$revisits2013, NA)
+pan_revisits$revisits2014 <- ifelse(pan_revisits$revisits2014 > 1, pan_revisits$revisits2014, NA)
+pan_revisits$revisits2015 <- ifelse(pan_revisits$revisits2015 > 1, pan_revisits$revisits2015, NA)
+p1<-ggplot(subset(pan_revisits, !is.na(revisits2013)), aes(x=X, y=Y, color=revisits2013)) +
+  geom_point(shape=19, size=2.5) + geom_point(data = centroidUTM_2, colour = "black",size=0.5) + 
+  geom_point(data = art_water_utm, colour = "yellow",size=2.5,shape=15) + 
+  scale_color_gradient(low="blue", high="red", limits=c(0, 130))
+p1 + labs(x = "Easting", y = "Northing", colour = "Revisits") + 
+  annotate("text", x=1085000, y=-2045000, label= "2013", size=6)
+p2<-ggplot(subset(pan_revisits, !is.na(revisits2014)), aes(x=X, y=Y, color=revisits2014)) + 
+  geom_point(shape=19, size=2.5) + geom_point(data = centroidUTM_2, colour = "black",size=0.5) + 
+  geom_point(data = art_water_utm, colour = "yellow",size=2.5,shape=15) + 
+  scale_color_gradient(low="blue", high="red", limits=c(0, 130))
+p2 + labs(x = "Easting", y = "Northing", colour = "Revisits") + 
+  annotate("text", x=1085000, y=-2045000, label= "2014", size=6)
+p3<-ggplot(subset(pan_revisits, !is.na(revisits2015)), aes(x=X, y=Y, color=revisits2015)) + 
+  geom_point(shape=19, size=2.5) + geom_point(data = centroidUTM_2, colour = "black",size=0.5) + 
+  geom_point(data = art_water_utm, colour = "yellow",size=2.5,shape=15) + 
+  scale_color_gradient(low="blue", high="red", limits=c(0, 130))
+p3 + labs(x = "Easting", y = "Northing", colour = "Revisits") + 
+  annotate("text", x=1085000, y=-2045000, label= "2015", size=6)
 
 
 
